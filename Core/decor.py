7b3bd4030e4eaa7fe67a14a0199626c7084d0149
Atsuho'Dev <130254969+Atsuho@users.noreply.github.com>
2023-04-21 09:16:53 +0200

    Add files via upload

---
+++ Core/decor.py
@@ -0,0 +1,41 @@
+from colorama import Fore
+
+res = Fore.RESET
+lye = Fore.LIGHTYELLOW_EX
+lre = Fore.LIGHTRED_EX
+
+
+menu = f"""
+{lye}
+[1] - trouver des infos avec un username
+[2] - trouver des infos sur une ipv4
+[3] - trouver des infos avec un numero de tel ou plaque d'imatriculation
+{lre}[4] - Exit {res}
+"""
+
+chsystem = f"""
+{lre}[!]choisit le systeme[!]{lye}
+[1] - Windows
+[2] - Linux/Termux{res}
+"""
+
+banner = f""" {lye}
+
+  
+███████╗███████╗██╗     ██╗███╗   ███╗      ███╗   ███╗ █████╗ ███████╗████████╗███████╗██████╗ 
+██╔════╝██╔════╝██║     ██║████╗ ████║      ████╗ ████║██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗
+███████╗█████╗  ██║     ██║██╔████╔██║█████╗██╔████╔██║███████║███████╗   ██║   █████╗  ██████╔╝
+╚════██║██╔══╝  ██║     ██║██║╚██╔╝██║╚════╝██║╚██╔╝██║██╔══██║╚════██║   ██║   ██╔══╝  ██╔══██╗
+███████║███████╗███████╗██║██║ ╚═╝ ██║      ██║ ╚═╝ ██║██║  ██║███████║   ██║   ███████╗██║  ██║
+╚══════╝╚══════╝╚══════╝╚═╝╚═╝     ╚═╝      ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
+                                                                                                
+                   ╔════════════════════════════════════════╗
+                   ║   < creer par atsuho x inquisiteur >   ║
+                   ╚════════════════════════════════════════╝   
+                                      
+"""
+
+sher_menu = f"""{lye}
+[1] - infos sur un numero de telephone
+[2] - infos sur une plaque d'imatriculation{res}
+"""
---
+++ README.md
@@ -0,0 +1,20 @@
+# selim-master
+🧥🔍 simple outils permettant de chercher des informations sur une personne devlopper en python
+
+# Functions:
+1. Chercher des infos a partir d'un numero de telephone
+2. trouver des infos avec une plaque d'imatriculation
+3. trouver des infos avec un nom d'utilisateur
+4. trouver des infos avec une ipv4
+
+# Installation:
+``git clone https://github.com/inquisit3ur/selim-master/``
+
+``cd selim-master``
+
+``pip3 install requirements.txt``
+
+``python selim.py``
+
+# About:
+1. Creer par inquisiteur & atsuho
